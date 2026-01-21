f4c87c74bf1258f5f085d8c5b8da0dedbbcbbccb
Malin Widberg <malin.widberg@gmail.com>
2026-01-21 13:12:23 +0100

    Fixar ApodView-komponenten

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const routes = [
   { path: "/dagens-bild", component: DagensBild },
   { path: "/slumpad", component: SlumpadBild },
   { path: "/datum", component: DatumValjare },
-  { path: "/datum/:date", component: ApodDatum, props: true },
+  { path: "/datum/:date", component: ApodView, props: true },
   { path: "/favoriter", component: Favoriter },
   { path: "/fakta", component: Fakta },
   { path: "/live", component: LiveEarth }
